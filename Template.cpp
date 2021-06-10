deb26503014ecbca5260bf3a283ccbf50789d6ff
Avantgarde95 <95phm@kaist.ac.kr>
2021-06-10 19:23:20 +0900

    Add CRTP example

--- Template.cpp
+++ Template.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 namespace CompileTimeComputation {
 
@@ -22,10 +23,52 @@ void run() {
     std::cout << n << "th fibonacci number: " << fibonacci<n>() << "\n";
 }
 
+}
+
+namespace CRTP {
+
+template <typename Derived>
+class Singleton {
+public:
+    static const Derived* getInstance() {
+        if (m_instance == nullptr) {
+            m_instance = new Derived();
+        }
+
+        return m_instance;
+    }
+
+private:
+    static Derived* m_instance;
 };
+
+template <typename Derived>
+Derived* Singleton<Derived>::m_instance = nullptr;
+
+class Manager : public Singleton<Manager> {
+public:
+    Manager() {
+        m_status = "Manager is created!";
+    }
+
+public:
+    const std::string& getStatus() const {
+        return m_status;
+    }
+
+private:
+    std::string m_status;
+};
+
+void run() {
+    std::cout << Manager::getInstance()->getStatus() << "\n";
+}
+
+}
 
 int main() {
     CompileTimeComputation::run();
+    CRTP::run();
 
     return 0;
 }
