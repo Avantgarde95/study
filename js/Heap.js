4175c5deadd63cce1e4ee7fcaed8f0ad0ea21a45
Avantgarde95 <mathematicianscott@gmail.com>
2021-09-22 12:55:06 +0900

    [feat] Implement heap removal operation
    * Remove the root from the heap
      * If heap.length = 0 -> Don't do anything
      * If heap.length = 1 -> Clear the heap
      * Else
        * Put the last value on the root
        * Heapify

--- js/Heap.js
+++ js/Heap.js
@@ -3,7 +3,8 @@ const { measureAndPrintJob } = require('./Common');
 function insertValue(heap, value, compare) {
     heap.push(value);
 
-    let currentIndex = heap.length - 1;
+    const heapSize = heap.length;
+    let currentIndex = heapSize - 1;
 
     while (currentIndex > 0) {
         const currentValue = heap[currentIndex];
@@ -21,26 +22,89 @@ function insertValue(heap, value, compare) {
     }
 }
 
+function removeRoot(heap, compare) {
+    const heapSize = heap.length;
+
+    if (heapSize <= 0) {
+        return;
+    } else if (heapSize === 1) {
+        heap.pop();
+        return;
+    }
+
+    heap[0] = heap.pop();
+
+    let currentIndex = 0;
+
+    while (true) {
+        const child1Index = currentIndex * 2 + 1;
+
+        if (child1Index >= heapSize) {
+            break;
+        }
+
+        const currentValue = heap[currentIndex];
+        const child1Value = heap[child1Index];
+
+        let child2Index = currentIndex * 2 + 2;
+
+        if (child2Index >= heapSize) {
+            child2Index = child1Index;
+        }
+
+        const child2Value = heap[child2Index];
+        let minChildIndex;
+        let minChildValue;
+
+        if (compare(child1Value, child2Value) <= 0) {
+            minChildIndex = child1Index;
+            minChildValue = child1Value;
+        } else {
+            minChildIndex = child2Index;
+            minChildValue = child2Value;
+        }
+
+        if (compare(currentValue, minChildValue) > 0) {
+            heap[currentIndex] = minChildValue;
+            heap[minChildIndex] = currentValue;
+            currentIndex = minChildIndex;
+        } else {
+            break;
+        }
+    }
+}
+
 // =========================================
 
 measureAndPrintJob('Min heap test', () => {
     const heap = [];
-    const values = [3, 2, 10, 8, 6, 4];
+    const values = [3, 2, 3, 8, 6, 4];
+    const compare = (v1, v2) => v1 - v2;
 
     values.forEach(value => {
-        insertValue(heap, value, (v1, v2) => v1 - v2);
+        insertValue(heap, value, compare);
+        console.log(`Insert ${value} -> [${heap.join(', ')}]`);
     });
 
-    console.log(heap);
+    for (let i = 0; i < 5; i++) {
+        removeRoot(heap, compare);
+        console.log(`Delete root -> [${heap.join(', ')}]`);
+    }
 });
 
 measureAndPrintJob('Max heap test', () => {
     const heap = [];
-    const values = [3, 2, 10, 8, 6, 4];
+    const values = [3, 2, 3, 8, 6, 4];
+
+    const compare = (v1, v2) => v2 - v1;
 
     values.forEach(value => {
-        insertValue(heap, value, (v1, v2) => v2 - v1);
+        insertValue(heap, value, compare);
+        console.log(`Insert ${value} -> [${heap.join(', ')}]`);
     });
 
-    console.log(heap);
+    for (let i = 0; i < 5; i++) {
+        removeRoot(heap, compare);
+        console.log(`Delete root -> [${heap.join(', ')}]`);
+    }
 });
